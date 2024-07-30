492efa555163327dbc48e9037fc3b3f672a2e7dd
dr250385 <dr250385@WIN-10M20X2>
2024-07-30 09:46:35 +0200

    Making and comparing choices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import SingleCard from './components/SingleCard';
 
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -25,6 +27,31 @@ function App() {
     setTurns(0);
   };
 
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log('those cards match');
+        resetTurn();
+      } else {
+        console.log('those cards do not match');
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns++);
+  };
+
   return (
     <div className='App'>
       <h1>Magic Match</h1>
@@ -32,7 +59,11 @@ function App() {
 
       <div className='card-grid'>
         {cards.map((card) => (
-          <SingleCard card={card} key={card.id}></SingleCard>
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+          ></SingleCard>
         ))}
       </div>
     </div>
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,11 +1,20 @@
-import './SIngleCards.scss';
+import './SIngleCard.scss';
 
-export default function SingleCard({ card: { src } }) {
+export default function SingleCard({ card, card: { src }, handleChoice }) {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
   return (
     <div className='card'>
       <div>
         <img className='front' src={src} alt='card front' />
-        <img className='back' src='/img/cover.png' alt='card back' />
+        <img
+          className='back'
+          src='/img/cover.png'
+          alt='card back'
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
